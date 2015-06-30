f2b5207b2784e68809aed8c678b53533cd82aa0d
artven <rafal.prusak@gmail.com>
2015-06-30 18:34:19 +0200

    Dodano klasę implementującą wyodrębnianie tła z obrazu.

---
+++ algorithms/bgsub.py
@@ -0,0 +1,56 @@
+__author__ = 'rafal'
+
+import cv2
+
+
+class BackgroundSubtractor:
+    MOG2, KNN = 0, 1
+
+    __MOG2 = cv2.createBackgroundSubtractorMOG2
+    __KNN = cv2.createBackgroundSubtractorKNN
+    __subtractor = (__MOG2, __KNN)
+
+    def __init__(self, subtype):
+        """
+        :param subtype: Typ silnika wyodrębniania tła - BackgroundSubtractor.MOG2/KNN
+        """
+
+        self.__fgbg = cv2.createBackgroundSubtractorMOG2() if subtype == 0 else cv2.createBackgroundSubtractorKNN()
+        self.__run = True
+        self.__lastFrame = None
+
+    def apply(self, frame, op=cv2.MORPH_OPEN, kernel=cv2.MORPH_ELLIPSE, kernelsize=(3, 3),
+              mediansize=3, dilateiter=1):
+        """Uzyskiwanie tła na podstawie klatki.
+        Funckja przetwarza kolejne klatki, wyodrębnia tło na podstawie sumy ważonej poprzednich klatek.
+        Wynik wyodrębniania tła jest poddawany obróbce(morfologiczna->mediana->dylatacja),
+        w celu wyraźnej separacji wyodrębnionych obiektów pierwszoplanowych.
+        :param frame: Ramka obrazu wejściowego.
+        :param op: Typ operacji morfologicznej - cv2.MORPH_*.
+        :param kernel: Jądro przekształcenia morfologicznego.
+        :param kernel: Jądro przekształcenia morfologicznego.
+        :param mediansize: Rozmiar filtru medianowego.
+        :param dilateiter: Liczba wykonywanych dylatacji obrazu.
+        :return: Binarna ramka obrazu z wyodrębnionym tłem i obiektami.
+        """
+
+        ker = cv2.getStructuringElement(kernel, kernelsize)
+        if not self.__run:
+            return self.__lastFrame
+
+        newframe = self.__fgbg.apply(frame)
+        morphframe = cv2.morphologyEx(newframe, op, ker)
+        medianframe = cv2.medianBlur(morphframe, mediansize)
+        dilatframe = cv2.dilate(medianframe, kernel, iterations=dilateiter)
+        self.__lastFrame = dilatframe
+
+        return self.__lastFrame
+
+    def control(self, key):
+        """Sterowanie procesem wyodrębniania.
+        Funkcja pozwala włączać/wyłączać proces substrakcji poprzez przekzanie klawisza.
+        Klawisz 's' spowoduje zatrzymanie operacji.
+        :param key: Klawisz(znak) sterujący.
+        """
+
+        self.__run = not self.__run if key == ord('s') else self.__run
