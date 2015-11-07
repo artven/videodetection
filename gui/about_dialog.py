3f1f7643a838e5311a07d7f0138df188e8d810c5
artven <rafal.prusak@gmail.com>
2015-11-07 03:09:16 +0100

    Dodano okienko wyświetlające informację o programie.

---
+++ gui/about_dialog.py
@@ -0,0 +1,22 @@
+__author__ = 'rafal'
+
+from gi.repository import Gtk
+
+class AboutDialog:
+    def __init__(self):
+        self.glade_file = "about_dialog.glade"
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file(self.glade_file)
+        self.builder.connect_signals(self)
+        self.window = self.builder.get_object("about_dialog")
+
+    def show(self):
+        self.window.show()
+
+    def on_about_dialog_response(self, object, data=None):
+        self.window.hide()
+
+if __name__ == "__main__":
+    dialog = AboutDialog()
+    dialog.show()
+    Gtk.main()
