7115be0094d1758c69d64eef21eb31cf04401ade
artven <rafal.prusak@gmail.com>
2015-12-04 17:30:54 +0100

    Zmiana sposobu urchamiania programu.

--- run_mainwindow.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-__author__ = 'rafal'
-
-from gi.repository import Gtk
-from gui.window_view import ProgramView
-
-if __name__ == "__main__":
-    window = ProgramView()
-    Gtk.main()
